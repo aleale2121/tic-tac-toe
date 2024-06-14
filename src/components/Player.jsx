c77ad8fdde60b90260b701cbc911c5512fd78c47
alefew <alefewyimer4@gmail.com>
2024-06-14 14:58:26 +0300

    conditional content

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,26 @@
-export default function Player({name,symbol}) {
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(!isEditing);
+  }
+  let playerName = <span className="player-name">{name}</span>;
+  let btnCaption = "Edit";
+
+  if (isEditing) {
+    playerName = <input type="text" value={name} />;
+    btnCaption = "Save";
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
-        <span className="player-sysmbol">{symbol}</span>
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{btnCaption}</button>
     </li>
   );
 }
