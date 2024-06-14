0c5983f38b9c149ca0160a973d677e641df4c56c
alefew <alefewyimer4@gmail.com>
2024-06-14 17:28:42 +0300

    driving state and use computed values

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,29 +1,25 @@
-import { useState } from "react";
-
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
-  function handleSelectSquare(rowIndex, colIdx) {
-    setGameBoard((prevGameBoard) => {
-      const updatedBoard = [...prevGameBoard.map((innerArr) => [...innerArr])];
-      updatedBoard[rowIndex][colIdx] = activePlayerSymbol;
-      return updatedBoard;
-    });
-    onSelectSquare();
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initialGameBoard;
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
   }
+
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
-        <li key={rowIndex}>
+      {gameBoard.map((row, rowIdx) => (
+        <li key={rowIdx}>
           <ol>
             {row.map((playerSymbol, colIdx) => (
               <li key={colIdx}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIdx)}>
+                <button onClick={() => onSelectSquare(rowIdx, colIdx)}>
                   {playerSymbol}
                 </button>
               </li>
